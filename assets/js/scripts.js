dd6205d9dab2ceeb3ba114e073c7f05611aaea3e
Daniel Seiler <55411968+dannyseiler@users.noreply.github.com>
2021-06-22 21:17:03 -0300

    Subiendo TP final
    Primera versión del Trabajo Práctico, Módulo 2 del Curso Desarrollo Web de Fundación Telefónica.

---
+++ assets/js/scripts.js
@@ -0,0 +1,11 @@
+jQuery("document").ready(function ($) {
+  var menuBtn = $(".menu-icon"),
+    menu = $(".navbar ul");
+  menuBtn.click(function () {
+    if (menu.hasClass("show")) {
+      menu.removeClass("show");
+    } else {
+      menu.addClass("show");
+    }
+  });
+});
